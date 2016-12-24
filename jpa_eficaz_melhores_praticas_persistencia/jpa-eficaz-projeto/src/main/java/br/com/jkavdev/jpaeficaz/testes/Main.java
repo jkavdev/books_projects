f9cee29d92dc92a53d7bb7bcec5a688082c015d3
jkavdev <jhonatankolen@gmail.com>
2016-12-24 00:12:30 -0200

    Incluindo entidades em um arquivo de mapeamento

--- jpa_eficaz_melhores_praticas_persistencia/jpa-eficaz-projeto/src/main/java/br/com/jkavdev/jpaeficaz/testes/Main.java
+++ jpa_eficaz_melhores_praticas_persistencia/jpa-eficaz-projeto/src/main/java/br/com/jkavdev/jpaeficaz/testes/Main.java
@@ -4,12 +4,16 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.jkavdev.jpaeficaz.modelo.Musica;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpaEficazPU");
 		EntityManager manager = factory.createEntityManager();
+		
+		manager.find(Musica.class, "1");
 
 		manager.close();
 		factory.close();
